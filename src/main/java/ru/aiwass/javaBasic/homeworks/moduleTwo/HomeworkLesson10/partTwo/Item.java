1a5fc2a312a667646cbc4a28131e2db05c60cdc0
EvaAiwass <evawhite400@gmail.com>
2023-11-02 12:52:41 +0700

    Fixed homework 10

--- src/main/java/ru/aiwass/javaBasic/homeworks/moduleTwo/HomeworkLesson10/partTwo/Item.java
+++ src/main/java/ru/aiwass/javaBasic/homeworks/moduleTwo/HomeworkLesson10/partTwo/Item.java
@@ -6,11 +6,6 @@ public class Item {
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Item(String name) {
         this.name = name;
     }

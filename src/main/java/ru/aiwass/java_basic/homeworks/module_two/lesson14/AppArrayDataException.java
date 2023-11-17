b994e46a03deeb5f4b7ebcdd0c3cb0349092680b
Eva Aiwass <Evawhite400@gmail.com>
2023-11-17 16:30:29 +0700

    Add homework lesson 14.

---
+++ src/main/java/ru/aiwass/java_basic/homeworks/module_two/lesson14/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.aiwass.java_basic.homeworks.module_two.lesson14;
+
+public class AppArrayDataException extends Exception {
+    public AppArrayDataException(String message) {
+        super(message);
+    }
+}

53a2fa69142459d95d9cf078761dcee1f8acae00
Eva Aiwass <Evawhite400@gmail.com>
2024-01-17 20:42:28 +0700

    Added homework lesson 27

---
+++ src/main/java/ru/aiwass/java_basic/homeworks/module_4/lesson27/Box.java
@@ -0,0 +1,34 @@
+package ru.aiwass.java_basic.homeworks.module_4.lesson27;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+    List<T> fruits;
+
+    public Box() {
+        this.fruits = new ArrayList<>();
+    }
+
+    public void add(T... fruits) {
+        this.fruits.addAll(Arrays.asList(fruits));
+    }
+
+    public double calculateWeight() {
+        double totalWeight = 0;
+        for (T fruit : fruits) {
+            totalWeight += fruit.getWeight();
+        }
+        return totalWeight;
+    }
+
+    public boolean compare(Box<?> anotherBox) {
+        return Math.abs(this.calculateWeight() - anotherBox.calculateWeight()) < 0.0001;
+    }
+
+    public void transferFruits(Box<? super T> anotherBox) {
+        anotherBox.fruits.addAll(this.fruits);
+        this.fruits.clear();
+    }
+}
---
+++ src/main/java/ru/aiwass/java_basic/homeworks/module_4/lesson27/Fruit.java
@@ -0,0 +1,14 @@
+package ru.aiwass.java_basic.homeworks.module_4.lesson27;
+
+public abstract class Fruit {
+
+    private final double weight;
+    public Fruit(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+}
